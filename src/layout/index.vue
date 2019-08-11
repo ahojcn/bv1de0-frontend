6adab0fabf1a6a7b4ea73085f0993f8c380e0a33
ahojcn <hanoi_ahoj@icloud.com>
2019-08-11 11:28:20 +0800

    更新：首页布局完成。

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -26,20 +26,68 @@
     <el-main>
 
       <el-row>
-        <el-col :span="8" v-for="video in video_data" :key="video.id" style="padding-bottom: 1%">
-          <el-card shadow="hover" :span="8">
-            <el-row :gutter="12">
-              <el-col :span="8">
-                标题：{{video.title}}<br>
-                作者：{{video.author}}<br>
-              </el-col>
-              <el-col :span="16">
-                <video src="http://127.0.0.1:8000/static/media/video/QQ20190726-210821-HD.mp4" style="width: 100px;"></video>
+        <el-col :span="18">
+          <el-card shadow="never">
+            <div slot="header" class="clearfix">
+              <span>
+                最新视频
+                <i class="el-icon-video-play"></i>
+              </span>
+            </div>
+
+            <el-row>
+              <el-col :span="8" v-for="video in video_data" :key="video.id" style="padding-bottom: 1%">
+                <el-row>
+                  <el-col :span="23">
+                    <el-card :body-style="{ padding: '0px' }" shadow="hover">
+                      <video src="http://127.0.0.1:8000/static/media/video/QQ20190726-210821-HD.mp4"
+                             class="image"></video>
+                      <div style="padding: 14px;">
+                        <span>{{video.title}}</span>
+                        <div class="bottom clearfix">
+                          <time class="time">{{ video.categories }}</time>
+                          <el-button type="text" class="button" @click="goToVideo(video)">去瞅瞅</el-button>
+                        </div>
+                      </div>
+                    </el-card>
+                  </el-col>
+                  <el-col :span="1"> </el-col>
+                </el-row>
               </el-col>
             </el-row>
+
           </el-card>
         </el-col>
+        <el-col :span="6">
+          <el-row>
+            <el-card shadow="hover">
+              <div slot="header" class="clearfix">
+              <span>
+                最新留言
+                <i class="el-icon-video-play"></i>
+              </span>
+              </div>
+            </el-card>
+          </el-row>
+          <el-row>
+            <el-card shadow="hover">
+              <div slot="header" class="clearfix">
+              <span>
+                新成员
+                <i class="el-icon-video-play"></i>
+              </span>
+              </div>
+
+              <div>
+                <span>青春是一个短暂的美梦, 当你醒来时, 它早已消失无踪</span>
+                <el-divider></el-divider>
+                <span>少量的邪恶足以抵消全部高贵的品质, 害得人声名狼藉</span>
+              </div>
+            </el-card>
+          </el-row>
+        </el-col>
       </el-row>
+
     </el-main>
 
 
@@ -61,6 +109,9 @@
     methods: {
       handleSelect(key, keyPath) {
         console.log(key, keyPath);
+      },
+      goToVideo(video) {
+        alert(video.id)
       }
     },
     mounted() {
@@ -82,5 +133,35 @@
     top: 0;
     left: 0;
     z-index: 200;
+  }
+
+  .time {
+    font-size: 13px;
+    color: #999;
+  }
+
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
+
+  .button {
+    padding: 0;
+    float: right;
+  }
+
+  .image {
+    width: 100%;
+    display: block;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+
+  .clearfix:after {
+    clear: both
   }
 </style>
